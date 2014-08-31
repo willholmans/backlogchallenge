2265cdee4ae582c2ba56eaff1e1cee78e1a8f9ef
Will Holmans <willholmans@gmail.com>
2014-08-31 15:32:59 +0000

    Unnecessary comments and methods removed

--- app/controllers/BacklogController.php
+++ app/controllers/BacklogController.php
@@ -2,11 +2,6 @@
 
 class BacklogController extends \BaseController {
 
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
 	public function index()
 	{
 		$backlog = Backlog::orderBy('priority')->get();	
@@ -14,23 +9,6 @@ class BacklogController extends \BaseController {
 		return $backlog->toJson();
 	}
 
-
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-		return View::make('backlog.create');
-	}
-
-
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
 	public function store()
 	{
 		$rules = array(
@@ -56,13 +34,6 @@ class BacklogController extends \BaseController {
 				'success' => 'woop'));
 	}
 
-
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
 	public function show($id)
 	{
 		$backlog = Backlog::findOrFail($id);
@@ -70,25 +41,6 @@ class BacklogController extends \BaseController {
 		return $backlog->toJson();
 	}
 
-
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-		//
-	}
-
-
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
 	public function update($id)
 	{
 		$rules = array(
@@ -117,13 +69,6 @@ class BacklogController extends \BaseController {
 			'success' => 'woop'));
 	}
 
-
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
 	public function destroy($id)
 	{
 		$backlog = Backlog::findOrFail($id);
